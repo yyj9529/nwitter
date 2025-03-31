976a95d6287c3deea5d026ad0f52225388228f80
yyj9529 <garethgates7@naver.com>
2025-04-01 02:53:06 +0900

    layout page

--- nwitter/src/components/layout.tsx
+++ nwitter/src/components/layout.tsx
@@ -1,10 +1,83 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { auth } from "../firebase";
+
+const Wrapper = styled.div`
+    display: grid;
+    gap : 20px;
+    grid-template-rows : 1fr 4fr;
+    height  : 100%;
+    padding : 50px; 0px;
+    width : 100%;
+    max-width : 860px;
+`;
+
+const Menu = styled.div`
+    display : flex;
+    flex-direction : column;
+    align-items : center;
+    gap : 20px;
+`;
+const MenuItem = styled.div`
+    cursor : pointer;
+    display : flex;
+    align-items : center;
+    justify-content : center;
+    border : 2px solid white;
+    height : 50px;
+    width : 50px;
+    border-radius : 50%;
+    svg{
+        width : 20px;
+        fill : white;
+    }
+    &.log-out{
+        border-color : tomato;
+        svg{
+            fill : tomato;
+        }
+    }
+`;
+
 
 export default function Layout(){
+    
+    const navigate = useNavigate();
+    const onLogOut = async () =>{
+        const ok = confirm("Are you sure you want to log out?");
+
+        if(ok){
+            await auth.signOut();
+            navigate("/login");
+        }
+    }
+
     return(
         <>
-            <h2>layout</h2>
-            <Outlet />
+            <Wrapper>
+                <Menu>
+                    <Link to="/">
+                        <MenuItem>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                            </svg>
+                        </MenuItem>
+                    </Link>
+                    <Link to="/profile">
+                        <MenuItem>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                            </svg>
+                        </MenuItem>
+                    </Link>
+                    <MenuItem onClick={onLogOut} className="log-out">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                        </svg>
+                    </MenuItem>
+                </Menu>
+                <Outlet />
+            </Wrapper>
         </>
     );
 }
